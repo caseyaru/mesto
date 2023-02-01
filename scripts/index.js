56c89f3b8904cd1a79875d556d3bc4f36ee8b105
Casey <arramine@mail.ru>
2023-02-01 11:17:54 +0300

    hotfix/после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,36 @@
-// ОТКРЫТИЕ-ЗАКРЫТИЕ ПОПАПА 
-
-const submitButton = document.querySelector('.profile__edit-button');
+// для всего попапа
+const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close');
 
-function toggleOpenPopup () {
-    popup.classList.toggle('popup_opened');
-  };
-
-function handleSubmitButton () {
-    toggleOpenPopup();
-}
-function handleCloseButton () {
-    toggleOpenPopup();
-}
-
-function handleOverlayClick (event) {
-    if (event.target === event.currentTarget) {
-      toggleOpenPopup();
-    }
-  };
-
-submitButton.addEventListener ('click', handleSubmitButton);
-closeButton.addEventListener ('click', handleCloseButton);
-popup.addEventListener('click', handleOverlayClick);
-
-// ОТПРАВКА ФОРМЫ
-
+// для формы
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__text');
+let nameInput = document.querySelector('.popup__field_type_name');
+let jobInput = document.querySelector('.popup__field_type_text');
 
 let nameStart = document.querySelector('.profile__name');
 let jobStart = document.querySelector('.profile__text');
 
-console.log(formElement, nameInput, jobInput, nameStart, jobStart); // всё работает
+// открытие-закрытие
+function handleProfileEditButton () {
+  popup.classList.add('popup_opened');
+  nameInput.value = nameStart.textContent;
+  jobInput.value = jobStart.textContent;
+}
 
+function handleCloseButton () {
+  popup.classList.remove('popup_opened');
+}
+
+profileEditButton.addEventListener ('click', handleProfileEditButton);
+closeButton.addEventListener ('click', handleCloseButton);
+
+// отправка формы
 function handleFormSubmit (evt) {
     evt.preventDefault(); // дефолт отправки, сейчас не важно
 
     nameStart.textContent = nameInput.value;
     jobStart.textContent = jobInput.value;
-
-    nameInput.value = "Жак-Ив Кусто";
-    jobInput.value = "Исследователь океана";
 
     handleCloseButton();
 }
